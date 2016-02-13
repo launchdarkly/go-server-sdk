a6604680f45f5b4697377df6a9b84be1281e4137
John Kodumal <john@launchdarkly.com>
2016-02-12 20:24:19 -0800

    Panic for now if we try to use the polling processor

--- polling.go
+++ polling.go
@@ -5,14 +5,15 @@ type pollingProcessor struct {
 
 // TODO
 func newPollingProcessor(apiKey string, config Config, store FeatureStore, requestor *requestor) updateProcessor {
-	return nil
+	panic("Can't get there from here")
 }
 
 // TODO
 func (pp *pollingProcessor) start(ch <-chan bool) {
+	panic("Can't get there from here")
 }
 
 // TODO
 func (pp *pollingProcessor) close() {
-
+	panic("Can't get there from here")
 }
