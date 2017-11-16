3e8275c519d6cae940339f7bd4c64133f9f78e48
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2017-11-16 13:58:23 -0800

    Add script to udpate version on release. (#15)

---
+++ scripts/release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This script updates the version for the ldclient.go
+
+set -uxe
+echo "Starting go-client release."
+
+VERSION=$1
+
+#Update version in ldclient.go
+LDCLIENT_GO_TEMP=./ldclient.go.tmp
+sed "s/const Version =.*/const Version = \"${VERSION}\"/g"  ldclient.go > ${LDCLIENT_GO_TEMP}
+mv ${LDCLIENT_GO_TEMP} ldclient.go
+
+echo "Done with go-client release"
+
