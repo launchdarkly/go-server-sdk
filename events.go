d59aeedea2a40cc150b2aa38e957c1e7a7a28d6d
Eli Bishop <this@errorbar.net>
2018-01-02 10:21:34 -0800

    do user scrubbing outside of lock

--- events.go
+++ events.go
@@ -152,18 +152,7 @@ func (ep *eventProcessor) sendEvent(evt Event) error {
 		return nil
 	}
 
-	ep.mu.Lock()
-	defer ep.mu.Unlock()
-
-	if ep.closed {
-		return nil
-	}
-	if len(ep.queue) >= ep.config.Capacity {
- 		return errors.New("Exceeded event queue capacity. Increase capacity to avoid dropping events.")
- 	}
- 	
-	scrubbedUser := scrubUser(evt.GetBase().User, ep.config.AllAttributesPrivate, ep.config.PrivateAttributeNames)
-
+	scrubbedUser := scrubUser(evt.GetBase().User, ep.config.AllAttributesPrivate, ep.config.PrivateAttributeNames) 	
 	var newEvent Event
 	switch evt := evt.(type) {
 	case FeatureRequestEvent:
@@ -178,6 +167,16 @@ func (ep *eventProcessor) sendEvent(evt Event) error {
 	default:
 		return errors.New("unknown event type")
 	}
+
+	ep.mu.Lock()
+	defer ep.mu.Unlock()
+
+	if ep.closed {
+		return nil
+	}
+	if len(ep.queue) >= ep.config.Capacity {
+ 		return errors.New("Exceeded event queue capacity. Increase capacity to avoid dropping events.")
+ 	}
 	ep.queue = append(ep.queue, newEvent)
 	return nil
 }
