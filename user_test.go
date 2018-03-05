4ae62f571e14a497406ca2d04b900b8d7a466d27
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-03-05 20:23:35 +0100

    Add test for NewUser

--- user_test.go
+++ user_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewUser(t *testing.T) {
+	user := NewUser("some-key")
+	k, _ := user.valueOf("key")
+	assert.Equal(t, "some-key", k)
+}
+
 func TestNewAnonymousUser(t *testing.T) {
 	user := NewAnonymousUser("some-key")
 
