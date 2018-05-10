d9f49448bb11a900a062bad0d4c632e0b99f683c
Eli Bishop <eli@launchdarkly.com>
2018-05-10 14:17:25 -0700

    bump version to v4

--- feature_store_test.go
+++ feature_store_test.go
@@ -3,8 +3,8 @@ package ldclient_test
 import (
 	"testing"
 
-	ld "gopkg.in/launchdarkly/go-client.v3"
-	ldtest "gopkg.in/launchdarkly/go-client.v3/shared_test"
+	ld "gopkg.in/launchdarkly/go-client.v4"
+	ldtest "gopkg.in/launchdarkly/go-client.v4/shared_test"
 )
 
 func makeInMemoryStore() ld.FeatureStore {
