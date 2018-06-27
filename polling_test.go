2d69a7c74b32f7c44ab825eaf335cf868963bc6b
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2018-06-27 11:28:24 -0700

    Fix polling processor to poll more than once (#90)
    Ticker was being stopped immediately.
    
    Add tests that ensure that we poll multiple times.
    Also test how we respond to different status codes in polling processor.

--- polling_test.go
+++ polling_test.go
@@ -1,6 +1,7 @@
 package ldclient
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -35,45 +36,26 @@ func TestPollingProcessor_ClosingItShouldNotBlock(t *testing.T) {
 	}
 }
 
-func TestPollingProcessor_401ShouldNotBlock(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusUnauthorized)
-	}))
-
-	defer ts.Close()
-
-	cfg := Config{
-		Logger:       log.New(ioutil.Discard, "", 0),
-		PollInterval: time.Minute,
-		BaseUri:      ts.URL,
-	}
-	req := newFakeRequestor(ts, cfg)
-	p := newPollingProcessor(cfg, req)
-
-	closeWhenReady := make(chan struct{})
-	p.Start(closeWhenReady)
-
-	select {
-	case <-closeWhenReady:
-	case <-time.After(time.Second):
-		assert.Fail(t, "Receiving 401 shouldn't block")
-	}
-}
-
 func TestPollingProcessor_Initialization(t *testing.T) {
+	polls := make(chan struct{}, 2)
+
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "/sdk/latest-all", r.URL.Path)
 		w.Write([]byte(`{"flags": {"my-flag": {"key": "my-flag", "version": 2}}, "segments": {"my-segment": {"key": "my-segment", "version": 3}}}`))
+		if len(polls) < cap(polls) {
+			polls <- struct{}{}
+		}
 	}))
 
 	defer ts.Close()
+	defer ts.CloseClientConnections()
 
 	store := NewInMemoryFeatureStore(log.New(ioutil.Discard, "", 0))
 
 	cfg := Config{
 		FeatureStore: store,
 		Logger:       log.New(ioutil.Discard, "", 0),
-		PollInterval: time.Minute,
+		PollInterval: time.Millisecond,
 		BaseUri:      ts.URL,
 	}
 	req := newFakeRequestor(ts, cfg)
@@ -97,6 +79,80 @@ func TestPollingProcessor_Initialization(t *testing.T) {
 	segment, err := store.Get(Segments, "my-segment")
 	if assert.NoError(t, err) {
 		assert.Equal(t, 3, segment.GetVersion())
+	}
+
+	for i := 0; i < 2; i++ {
+		select {
+		case <-polls:
+		case <-time.After(time.Second):
+			assert.Fail(t, "Expected 2 polls but only got %d", i)
+			return
+		}
+	}
+}
+
+func TestPollingProcessorRequestResponseCodes(t *testing.T) {
+	specs := []struct {
+		statusCode  int
+		recoverable bool
+	}{
+		{400, false},
+		{401, false},
+		{403, false},
+		{405, false},
+		{408, true},
+		{429, true},
+		{500, true},
+	}
+
+	for _, tt := range specs {
+		t.Run(fmt.Sprintf("status %d, recoverable %v", tt.statusCode, tt.recoverable), func(t *testing.T) {
+			polls := make(chan struct{}, 2)
+
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if len(polls) < cap(polls) {
+					polls <- struct{}{}
+				}
+				w.WriteHeader(tt.statusCode)
+			}))
+
+			defer ts.Close()
+			defer ts.CloseClientConnections()
+
+			cfg := Config{
+				Logger:       log.New(ioutil.Discard, "", 0),
+				PollInterval: time.Millisecond * 10,
+				BaseUri:      ts.URL,
+			}
+			req := newFakeRequestor(ts, cfg)
+			p := newPollingProcessor(cfg, req)
+			closeWhenReady := make(chan struct{})
+			p.Start(closeWhenReady)
+
+			if tt.recoverable {
+				// wait for two polls
+				for i := 0; i < 2; i++ {
+					select {
+					case <-polls:
+						t.Logf("Got poll attempt %d/2", i+1)
+					case <-closeWhenReady:
+						assert.Fail(t, "should not report ready")
+						break
+					case <-time.After(time.Second * 3):
+						assert.Fail(t, "failed to retry")
+						break
+					}
+				}
+			} else {
+				select {
+				case <-closeWhenReady:
+					assert.Len(t, polls, 1) // should be ready after a single attempt
+					assert.False(t, p.Initialized())
+				case <-time.After(time.Second):
+					assert.Fail(t, "channel was not closed immediately")
+				}
+			}
+		})
 	}
 }
 
