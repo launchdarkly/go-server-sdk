6ee84d8b643a5cc4b07c0596fdb66f3a9ae9305b
Eli Bishop <eli@launchdarkly.com>
2018-10-24 21:59:16 -0700

    break up a big function into several methods and a struct

--- ldfilewatch/watched_file_data_source.go
+++ ldfilewatch/watched_file_data_source.go
@@ -15,6 +15,16 @@ import (
 
 const retryDuration = time.Second
 
+type fileWatcher struct {
+	watcher  *fsnotify.Watcher
+	logger   ld.Logger
+	reload   func()
+	paths    []string
+	absPaths map[string]bool
+	retryCh  chan struct{}
+	closeCh  <-chan struct{}
+}
+
 // WatchFiles sets up a mechanism for FileDataSource to reload its source files whenever one of them has
 // been modified. Use it as follows:
 //
@@ -26,80 +36,102 @@ func WatchFiles(paths []string, logger ld.Logger, reload func(), closeCh <-chan 
 	if err != nil {
 		return fmt.Errorf("Unable to create file watcher: %s", err)
 	}
-
-	retryChannel := make(chan struct{}, 1)
-	scheduleRetry := func() {
-		time.AfterFunc(retryDuration, func() {
-			select {
-			case retryChannel <- struct{}{}: // don't need multiple retries so no need to block
-			default:
-			}
-		})
+	fw := &fileWatcher{
+		watcher:  watcher,
+		logger:   logger,
+		reload:   reload,
+		paths:    paths,
+		absPaths: make(map[string]bool),
+		retryCh:  make(chan struct{}, 1),
+		closeCh:  closeCh,
 	}
-	go func() {
-		for {
-			realPaths := map[string]bool{}
-			for _, p := range paths {
-				absDirPath := path.Dir(p)
-				realDirPath, err := filepath.EvalSymlinks(absDirPath)
-				if err != nil {
-					logger.Printf(`Unable to evaluate symlinks for "%s": %s`, absDirPath, err)
-					scheduleRetry()
-				}
+	go fw.run()
+	return nil
+}
 
-				realPath := path.Join(realDirPath, path.Base(p))
-				realPaths[realPath] = true
-				_ = watcher.Add(realPath) // ok if this doesn't find the file: we're still watching the directory
+func (fw *fileWatcher) run() {
+	for {
+		if err := fw.setupWatches(); err != nil {
+			fw.logger.Printf(err.Error())
+			fw.scheduleRetry()
+		}
 
-				if err = watcher.Add(realDirPath); err != nil {
-					logger.Printf(`Unable to watch directory "%s" for file "%s": %s`, realDirPath, p, err)
-					scheduleRetry()
-				}
+		// We do the reload here rather than after waitForEvents, even though that means there will be a
+		// redundant load when we first start up, because otherwise there's a potential race condition where
+		// file changes could happen before we had set up our file watcher.
+		fw.reload()
+
+		quit := fw.waitForEvents()
+		if quit {
+			return
+		}
+	}
+}
+
+func (fw *fileWatcher) setupWatches() error {
+	for _, p := range fw.paths {
+		absDirPath := path.Dir(p)
+		realDirPath, err := filepath.EvalSymlinks(absDirPath)
+		if err != nil {
+			return fmt.Errorf(`Unable to evaluate symlinks for "%s": %s`, absDirPath, err)
+		}
+
+		realPath := path.Join(realDirPath, path.Base(p))
+		fw.absPaths[realPath] = true
+		if err = fw.watcher.Add(realPath); err != nil {
+			return fmt.Errorf(`Unable to watch path "%s": %s`, realPath, err)
+		}
+		if err = fw.watcher.Add(realDirPath); err != nil {
+			return fmt.Errorf(`Unable to watch path "%s": %s`, realDirPath, err)
+		}
+	}
+	return nil
+}
+
+func (fw *fileWatcher) waitForEvents() bool {
+	for {
+		select {
+		case <-fw.closeCh:
+			err := fw.watcher.Close()
+			if err != nil {
+				fw.logger.Printf("Error closing Watcher: %s", err)
 			}
-
-			// We do the reload here rather than after WaitForUpdates, even though that means there will be a
-			// redundant load when we first start up, because otherwise there's a potential race condition where
-			// file changes could happen before we had set up our file watcher.
-			reload()
-
-		WaitForUpdates:
+			return true
+		case event := <-fw.watcher.Events:
+			if !fw.absPaths[event.Name] {
+				break
+			}
+			// Consume extra events
+		ConsumeExtraEvents:
 			for {
 				select {
-				case <-closeCh:
-					err := watcher.Close()
-					if err != nil {
-						logger.Printf("Error closing Watcher: %s", err)
-					}
-					return
-				case event := <-watcher.Events:
-					if realPaths[event.Name] {
-						// Consume extra events
-					ConsumeExtraEvents:
-						for {
-							select {
-							case <-watcher.Events:
-							default:
-								break ConsumeExtraEvents
-							}
-						}
-						break WaitForUpdates
-					}
-				case err := <-watcher.Errors:
-					logger.Println("ERROR: ", err)
-				case <-retryChannel:
-				ConsumeExtraRetries:
-					for {
-						select {
-						case <-retryChannel:
-						default:
-							break ConsumeExtraRetries
-						}
-					}
-					break WaitForUpdates
+				case <-fw.watcher.Events:
+				default:
+					break ConsumeExtraEvents
 				}
 			}
+			return false
+		case err := <-fw.watcher.Errors:
+			fw.logger.Println("ERROR: ", err)
+		case <-fw.retryCh:
+		ConsumeExtraRetries:
+			for {
+				select {
+				case <-fw.retryCh:
+				default:
+					break ConsumeExtraRetries
+				}
+			}
+			return false
 		}
-	}()
+	}
+}
 
-	return nil
+func (fw *fileWatcher) scheduleRetry() {
+	time.AfterFunc(retryDuration, func() {
+		select {
+		case fw.retryCh <- struct{}{}: // don't need multiple retries so no need to block
+		default:
+		}
+	})
 }
