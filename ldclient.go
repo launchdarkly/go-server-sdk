2fa8b5f84bd60cd59a1ea75d3e2044a5f9231bfa
LaunchDarklyCI <dev@launchdarkly.com>
2018-11-15 01:10:04 +0000

    Preparing for release of version 4.5.0

--- ldclient.go
+++ ldclient.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version is the client version
-const Version = "4.4.0"
+const Version = "4.5.0"
 
 // LDClient is the LaunchDarkly client. Client instances are thread-safe.
 // Applications should instantiate a single instance for the lifetime
