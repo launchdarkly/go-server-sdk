4b24a570264abdbb484599ed2e3dfda2f161fcf5
Eli Bishop <eli@launchdarkly.com>
2020-05-21 21:16:02 -0700

    (5.0) (optimization) don't create any event instances in NoEvents mode

--- ldcomponents/no_events.go
+++ ldcomponents/no_events.go
@@ -22,3 +22,10 @@ func NoEvents() interfaces.EventProcessorFactory {
 func (f nullEventProcessorFactory) CreateEventProcessor(context interfaces.ClientContext) (ldevents.EventProcessor, error) {
 	return ldevents.NewNullEventProcessor(), nil
 }
+
+// This method implements a hidden interface in ldclient_events.go, as a hint to the SDK that this is
+// the stub implementation of EventProcessorFactory and therefore LDClient does not need to bother
+// generating events at all.
+func (f nullEventProcessorFactory) IsNullEventProcessorFactory() bool {
+	return true
+}
