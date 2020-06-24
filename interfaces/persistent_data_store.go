bda89a0b6c2fab7e0ca5780bf804703706a5d71f
Eli Bishop <eli@launchdarkly.com>
2020-06-24 13:03:02 -0700

    comment about data store tests

--- interfaces/persistent_data_store.go
+++ interfaces/persistent_data_store.go
@@ -17,6 +17,11 @@ import "io"
 // The SDK will log the exception and will assume that the data store is now in a non-operational
 // non-operational state; the SDK will then start polling IsStoreAvailable() to determine when the
 // store has started working again.
+//
+// Whenever a new implementation of this interface is written, it should have unit tests that use
+// testhelpers.PersistentDataStoreTestSuite. This ensures that all of the interface methods are exercised
+// consistently in various scenarios that might be encountered in real SDK usage. It is not necessary to
+// use a real LDClient to do these tests.
 type PersistentDataStore interface {
 	io.Closer
 
