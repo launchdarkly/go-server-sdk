1df5023d2acecac9a7d5c882177b59ef538cf38c
Eli Bishop <eli@launchdarkly.com>
2020-12-14 11:28:47 -0800

    5.1.0-alpha.1

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/gregjones/httpcache v0.0.0-20171119193500-2bcd89a1743f
 	github.com/kr/pretty v0.2.1 // indirect
-	github.com/launchdarkly/eventsource v1.4.3
+	github.com/launchdarkly/eventsource v1.6.2
 	github.com/launchdarkly/go-ntlm-proxy-auth v1.0.1
 	github.com/launchdarkly/go-test-helpers/v2 v2.2.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
@@ -15,8 +15,9 @@ require (
 	golang.org/x/sys v0.0.0-20200122134326-e047566fdf82 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/ghodss/yaml.v1 v1.0.0
-	gopkg.in/launchdarkly/go-sdk-common.v2 v2.0.1
-	gopkg.in/launchdarkly/go-sdk-events.v1 v1.0.0
-	gopkg.in/launchdarkly/go-server-sdk-evaluation.v1 v1.0.1
+	gopkg.in/launchdarkly/go-jsonstream.v1 v1.0.0-alpha.1
+	gopkg.in/launchdarkly/go-sdk-common.v2 v2.12.0-alpha.3
+	gopkg.in/launchdarkly/go-sdk-events.v1 v1.0.1-alpha.4
+	gopkg.in/launchdarkly/go-server-sdk-evaluation.v1 v1.1.0-alpha.2
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
---
+++ internal/datasource/streaming_data_source_events_test.go
@@ -0,0 +1,151 @@
+package datasource
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"gopkg.in/launchdarkly/go-server-sdk-evaluation.v1/ldbuilders"
+	"gopkg.in/launchdarkly/go-server-sdk.v5/internal/datakinds"
+	"gopkg.in/launchdarkly/go-server-sdk.v5/internal/sharedtest"
+)
+
+func TestParsePutData(t *testing.T) {
+	allDataJSON := `{
+ "flags": {
+  "flag1": {"key": "flag1", "version": 1},
+  "flag2": {"key": "flag2", "version": 2}
+ },
+ "segments": {
+  "segment1": {"key": "segment1","version": 3}
+ }
+}`
+	t.Run("valid", func(t *testing.T) {
+		input := []byte(`{"path": "/", "data": ` + allDataJSON + `}`)
+		expected := sharedtest.NewDataSetBuilder().
+			Flags(ldbuilders.NewFlagBuilder("flag1").Version(1).Build(),
+				ldbuilders.NewFlagBuilder("flag2").Version(2).Build()).
+			Segments(ldbuilders.NewSegmentBuilder("segment1").Version(3).Build()).
+			Build()
+
+		result, err := parsePutData(input)
+		require.NoError(t, err)
+
+		assert.Equal(t, "/", result.Path)
+		assert.Equal(t, sharedtest.NormalizeDataSet(expected), sharedtest.NormalizeDataSet(result.Data))
+	})
+
+	t.Run("missing path", func(t *testing.T) {
+		input := []byte(`{"data": {` + allDataJSON + `}}`)
+		_, err := parsePutData(input)
+		require.Error(t, err)
+	})
+
+	t.Run("missing data", func(t *testing.T) {
+		input := []byte(`{"path": "/"}`)
+		_, err := parsePutData(input)
+		require.Error(t, err)
+	})
+}
+
+func TestParsePatchData(t *testing.T) {
+	flag := ldbuilders.NewFlagBuilder("flagkey").Version(2).On(true).Build()
+	segment := ldbuilders.NewSegmentBuilder("segmentkey").Version(3).Included("x").Build()
+	flagJSON := `{"key": "flagkey", "version": 2, "on": true}`
+	segmentJSON := `{"key": "segmentkey", "version": 3, "included": ["x"]}`
+
+	t.Run("valid flag", func(t *testing.T) {
+		input := []byte(`{"path": "/flags/flagkey", "data": ` + flagJSON + `}`)
+		result, err := parsePatchData(input)
+		require.NoError(t, err)
+
+		assert.Equal(t, datakinds.Features, result.Kind)
+		assert.Equal(t, "flagkey", result.Key)
+		assert.Equal(t, sharedtest.FlagDescriptor(flag), result.Data)
+	})
+
+	t.Run("valid segment", func(t *testing.T) {
+		input := []byte(`{"path": "/segments/segmentkey", "data": ` + segmentJSON + `}`)
+		result, err := parsePatchData(input)
+		require.NoError(t, err)
+
+		assert.Equal(t, datakinds.Segments, result.Kind)
+		assert.Equal(t, "segmentkey", result.Key)
+		assert.Equal(t, sharedtest.SegmentDescriptor(segment), result.Data)
+	})
+
+	t.Run("valid but data property appears before path", func(t *testing.T) {
+		input := []byte(`{"data": ` + flagJSON + `, "path": "/flags/flagkey"}`)
+		result, err := parsePatchData(input)
+		require.NoError(t, err)
+
+		assert.Equal(t, datakinds.Features, result.Kind)
+		assert.Equal(t, "flagkey", result.Key)
+		assert.Equal(t, sharedtest.FlagDescriptor(flag), result.Data)
+	})
+
+	t.Run("unrecognized path", func(t *testing.T) {
+		input := []byte(`{"path": "/cats/lucy", "data": ` + flagJSON + `}`)
+		result, err := parsePatchData(input)
+		require.NoError(t, err)
+
+		assert.Nil(t, result.Kind)
+		assert.Equal(t, "", result.Key)
+	})
+
+	t.Run("missing path", func(t *testing.T) {
+		input := []byte(`{"data": ` + flagJSON + `}`)
+		_, err := parsePatchData(input)
+		require.Error(t, err)
+	})
+
+	t.Run("missing data", func(t *testing.T) {
+		input := []byte(`{"path": "/flags/flagkey"}`)
+		_, err := parsePatchData(input)
+		require.Error(t, err)
+	})
+}
+
+func TestParseDeleteData(t *testing.T) {
+	t.Run("valid flag", func(t *testing.T) {
+		input := []byte(`{"path": "/flags/flagkey", "version": 3}`)
+		result, err := parseDeleteData(input)
+		require.NoError(t, err)
+
+		assert.Equal(t, datakinds.Features, result.Kind)
+		assert.Equal(t, "flagkey", result.Key)
+		assert.Equal(t, 3, result.Version)
+	})
+
+	t.Run("valid segment", func(t *testing.T) {
+		input := []byte(`{"path": "/segments/segmentkey", "version": 4}`)
+		result, err := parseDeleteData(input)
+		require.NoError(t, err)
+
+		assert.Equal(t, datakinds.Segments, result.Kind)
+		assert.Equal(t, "segmentkey", result.Key)
+		assert.Equal(t, 4, result.Version)
+	})
+
+	t.Run("unrecognized path", func(t *testing.T) {
+		input := []byte(`{"path": "/cats/macavity", "version": 9}`)
+		result, err := parseDeleteData(input)
+		require.NoError(t, err)
+
+		assert.Nil(t, result.Kind)
+		assert.Equal(t, "", result.Key)
+	})
+
+	t.Run("missing path", func(t *testing.T) {
+		input := []byte(`{"version": 1}`)
+		_, err := parseDeleteData(input)
+		require.Error(t, err)
+	})
+
+	t.Run("missing version", func(t *testing.T) {
+		input := []byte(`{"path": "/flags/flagkey"}`)
+		_, err := parseDeleteData(input)
+		require.Error(t, err)
+	})
+}
