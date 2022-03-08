49bc9a0c3bcae3ec24f677e362caa6834db226de
Eli Bishop <eli@launchdarkly.com>
2022-03-08 11:03:26 -0800

    add comment

--- interfaces/client_context.go
+++ interfaces/client_context.go
@@ -27,5 +27,6 @@ type BasicConfiguration struct {
 	// ServiceEndpoints include any configured custom service URIs.
 	ServiceEndpoints ServiceEndpoints
 
+	// ApplicationInfo includes any configured application metadata.
 	ApplicationInfo ApplicationInfo
 }
