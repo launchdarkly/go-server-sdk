bb8cb0d9b158ed2ce95dc19a85387a1916feea40
Eli Bishop <eli@launchdarkly.com>
2022-04-29 17:48:12 -0700

    enable contract tests for polling mode

--- testservice/sdk_client_entity.go
+++ testservice/sdk_client_entity.go
@@ -193,8 +193,17 @@ func makeSDKConfig(config servicedef.SDKConfigParams, sdkLog ldlog.Loggers) ld.C
 			ret.ServiceEndpoints.Streaming = config.Streaming.BaseURI
 		}
 		builder := ldcomponents.StreamingDataSource()
-		if config.Streaming.InitialRetryDelayMs != nil {
-			builder.InitialReconnectDelay(time.Millisecond * time.Duration(*config.Streaming.InitialRetryDelayMs))
+		if config.Streaming.InitialRetryDelayMS != nil {
+			builder.InitialReconnectDelay(time.Millisecond * time.Duration(*config.Streaming.InitialRetryDelayMS))
+		}
+		ret.DataSource = builder
+	} else if config.Polling != nil {
+		if config.Polling.BaseURI != "" {
+			ret.ServiceEndpoints.Polling = config.Polling.BaseURI
+		}
+		builder := ldcomponents.PollingDataSource()
+		if config.Polling.PollIntervalMS != nil {
+			builder.PollInterval(time.Millisecond * time.Duration(*config.Polling.PollIntervalMS))
 		}
 		ret.DataSource = builder
 	}
