753419414552b725fb87f85fcf5d26043f05e391
Casey Waldren <cwaldren@launchdarkly.com>
2023-04-10 18:21:12 -0700

    make empty filter keys a build error on streaming/polling data source builders

--- ldcomponents/polling_data_source_builder_test.go
+++ ldcomponents/polling_data_source_builder_test.go
@@ -30,16 +30,29 @@ func TestPollingDataSourceBuilder(t *testing.T) {
 	})
 
 	t.Run("PayloadFilter", func(t *testing.T) {
-		s := PollingDataSource()
-		assert.Equal(t, "", s.filterKey)
+		t.Run("build succeeds with no payload filter", func(t *testing.T) {
+			s := PollingDataSource()
+			clientContext := makeTestContextWithBaseURIs("base")
+			_, err := s.Build(clientContext)
+			assert.NoError(t, err)
+		})
 
-		s.PayloadFilter("microservice-1")
-		assert.Equal(t, "microservice-1", s.filterKey)
+		t.Run("build succeeds with non-empty payload filter", func(t *testing.T) {
+			s := PollingDataSource()
+			clientContext := makeTestContextWithBaseURIs("base")
+			s.PayloadFilter("microservice-1")
+			_, err := s.Build(clientContext)
+			assert.NoError(t, err)
+		})
 
-		s.PayloadFilter("")
-		assert.Equal(t, "", s.filterKey)
+		t.Run("build fails with empty payload filter", func(t *testing.T) {
+			s := PollingDataSource()
+			clientContext := makeTestContextWithBaseURIs("base")
+			s.PayloadFilter("")
+			_, err := s.Build(clientContext)
+			assert.Error(t, err)
+		})
 	})
-
 	t.Run("CreateDefaultDataSource", func(t *testing.T) {
 		baseURI := "base"
 
