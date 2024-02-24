31ce8cc99bf3fcf4abec13e47f40306bd4fa5210
Casey Waldren <cwaldren@launchdarkly.com>
2024-02-23 16:12:59 -0800

    chore: remove releaser config

--- .ldrelease/update-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# This script updates the version for the ldclient.go
-
-set -ue
-
-SOURCE_FILE=./internal/version.go
-TEMP_FILE=${SOURCE_FILE}.tmp
-sed "s/const SDKVersion =.*/const SDKVersion = \"${LD_RELEASE_VERSION}\"/g" ${SOURCE_FILE} > ${TEMP_FILE}
-mv ${TEMP_FILE} ${SOURCE_FILE}
