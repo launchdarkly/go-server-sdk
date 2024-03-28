a54c2d132aeaec2bf5c714168bb280682a92bec7
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-03-28 15:07:24 -0700

    feat: Implement supporting types for hooks. (#125)
    This PR should be reviewed before other hooks PRs.
    
    This PR adds supporting types for hooks, but does not use them from the
    client.
    
    ---------
    
    Co-authored-by: Casey Waldren <cwaldren@launchdarkly.com>

---
+++ ldhooks/package_info.go
@@ -0,0 +1,5 @@
+// Package ldhooks allows for writing extensions to the LaunchDarkly client functionality for purposes such as
+// telemetry.
+//
+// A developer does not need to use this package in the typical operation of the LaunchDarkly SDK.
+package ldhooks
