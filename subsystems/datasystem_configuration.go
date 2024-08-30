0efda7adc381698479b310064a8366fecd5235e8
Casey Waldren <cwaldren@launchdarkly.com>
2024-08-29 18:00:41 -0700

    refactor persistent store config

--- subsystems/datasystem_configuration.go
+++ subsystems/datasystem_configuration.go
@@ -14,8 +14,16 @@ type SynchronizersConfiguration struct {
 	Secondary Synchronizer
 }
 
+type StoreMode int
+
+const (
+	StoreModeRead      = 0
+	StoreModeReadWrite = 1
+)
+
 type DataSystemConfiguration struct {
-	Store DataStore
+	Store     DataStore
+	StoreMode StoreMode
 	// Initializers obtain data for the SDK in a one-shot manner at startup. Their job is to get the SDK
 	// into a state where it is serving somewhat fresh values as fast as possible.
 	Initializers  []Initializer
