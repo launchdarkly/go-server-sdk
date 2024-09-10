64b8074c6d44f8fed95a6bb746232da42df0a4a6
Casey Waldren <cwaldren@launchdarkly.com>
2024-09-10 15:40:49 -0700

    revert changes to StreamingDataSourceBuilder, and make a V2 struct instead

--- ldcomponents/streaming_data_source_builder_v2.go
+++ ldcomponents/streaming_data_source_builder_v2.go
@@ -2,40 +2,38 @@ package ldcomponents
 
 import (
 	"errors"
-	"time"
-
 	"github.com/launchdarkly/go-sdk-common/v3/ldvalue"
 	"github.com/launchdarkly/go-server-sdk/v7/internal/datasource"
+	"github.com/launchdarkly/go-server-sdk/v7/internal/datasourcev2"
 	"github.com/launchdarkly/go-server-sdk/v7/internal/endpoints"
 	"github.com/launchdarkly/go-server-sdk/v7/subsystems"
+	"time"
 )
 
-// DefaultStreamingBaseURI is the default value for [StreamingDataSourceBuilder.BaseURI].
-const DefaultStreamingBaseURI = endpoints.DefaultStreamingBaseURI
-
-// DefaultInitialReconnectDelay is the default value for [StreamingDataSourceBuilder.InitialReconnectDelay].
-const DefaultInitialReconnectDelay = time.Second
-
-// StreamingDataSourceBuilder provides methods for configuring the streaming data source.
+// StreamingDataSourceBuilderV2 provides methods for configuring the streaming data source in v2 mode.
 //
-// See StreamingDataSource for usage.
-type StreamingDataSourceBuilder struct {
+// V2 uses the next generation streaming protocol. This method is not stable, and not subject to any backwards
+// compatibility guarantees or semantic versioning. It is not suitable for production usage.
+//
+// Do not use it.
+// You have been warned.
+type StreamingDataSourceBuilderV2 struct {
 	initialReconnectDelay time.Duration
 	filterKey             ldvalue.OptionalString
 }
 
-// StreamingDataSource returns a configurable factory for using streaming mode to get feature flag data.
+// StreamingDataSourceV2 returns a configurable factory for using streaming mode to get feature flag data.
 //
 // By default, the SDK uses a streaming connection to receive feature flag data from LaunchDarkly. To use the
 // default behavior, you do not need to call this method. However, if you want to customize the behavior of
-// the connection, call this method to obtain a builder, set its properties with the [StreamingDataSourceBuilder]
+// the connection, call this method to obtain a builder, set its properties with the [StreamingDataSourceBuilderV2]
 // methods, and then store it in the DataSource field of [github.com/launchdarkly/go-server-sdk/v7.Config]:
 //
 //	config := ld.Config{
-//	    DataSource: ldcomponents.StreamingDataSource().InitialReconnectDelay(500 * time.Millisecond),
+//	    DataSource: ldcomponents.StreamingDataSourceV2().InitialReconnectDelay(500 * time.Millisecond),
 //	}
-func StreamingDataSource() *StreamingDataSourceBuilder {
-	return &StreamingDataSourceBuilder{
+func StreamingDataSourceV2() *StreamingDataSourceBuilderV2 {
+	return &StreamingDataSourceBuilderV2{
 		initialReconnectDelay: DefaultInitialReconnectDelay,
 	}
 }
@@ -47,9 +45,9 @@ func StreamingDataSource() *StreamingDataSourceBuilder {
 // exponentially for any subsequent connection failures.
 //
 // The default value is [DefaultInitialReconnectDelay].
-func (b *StreamingDataSourceBuilder) InitialReconnectDelay(
+func (b *StreamingDataSourceBuilderV2) InitialReconnectDelay(
 	initialReconnectDelay time.Duration,
-) *StreamingDataSourceBuilder {
+) *StreamingDataSourceBuilderV2 {
 	if initialReconnectDelay <= 0 {
 		b.initialReconnectDelay = DefaultInitialReconnectDelay
 	} else {
@@ -66,13 +64,13 @@ func (b *StreamingDataSourceBuilder) InitialReconnectDelay(
 // a smaller, known subset - then a payload filter may be setup in LaunchDarkly, and the filter's key specified here.
 //
 // Evaluations for flags that aren't part of the filtered environment will return default values.
-func (b *StreamingDataSourceBuilder) PayloadFilter(filterKey string) *StreamingDataSourceBuilder {
+func (b *StreamingDataSourceBuilderV2) PayloadFilter(filterKey string) *StreamingDataSourceBuilderV2 {
 	b.filterKey = ldvalue.NewOptionalString(filterKey)
 	return b
 }
 
 // Build is called internally by the SDK.
-func (b *StreamingDataSourceBuilder) Build(context subsystems.ClientContext) (subsystems.DataSource, error) {
+func (b *StreamingDataSourceBuilderV2) Build(context subsystems.ClientContext) (subsystems.DataSource, error) {
 	filterKey, wasSet := b.filterKey.Get()
 	if wasSet && filterKey == "" {
 		return nil, errors.New("payload filter key cannot be an empty string")
@@ -87,7 +85,7 @@ func (b *StreamingDataSourceBuilder) Build(context subsystems.ClientContext) (su
 		InitialReconnectDelay: b.initialReconnectDelay,
 		FilterKey:             filterKey,
 	}
-	return datasource.NewStreamProcessor(
+	return datasourcev2.NewStreamProcessor(
 		context,
 		context.GetDataSourceUpdateSink(),
 		cfg,
@@ -95,7 +93,7 @@ func (b *StreamingDataSourceBuilder) Build(context subsystems.ClientContext) (su
 }
 
 // DescribeConfiguration is used internally by the SDK to inspect the configuration.
-func (b *StreamingDataSourceBuilder) DescribeConfiguration(context subsystems.ClientContext) ldvalue.Value {
+func (b *StreamingDataSourceBuilderV2) DescribeConfiguration(context subsystems.ClientContext) ldvalue.Value {
 	return ldvalue.ObjectBuild().
 		SetBool("streamingDisabled", false).
 		SetBool("customStreamURI",
