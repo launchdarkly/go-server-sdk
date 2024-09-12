155b24adbf1527a4489231fa6088ce8bbc00e8af
Casey Waldren <cwaldren@launchdarkly.com>
2024-09-12 14:46:24 -0700

    add dedicated FDv2 streaming protocol builder for unit tests

--- ldclient_end_to_end_fdv2_test.go
+++ ldclient_end_to_end_fdv2_test.go
@@ -1,8 +1,8 @@
 package ldclient
 
 import (
-	"encoding/json"
 	"github.com/launchdarkly/go-server-sdk/v7/internal/datasourcev2"
+	"github.com/launchdarkly/go-server-sdk/v7/testhelpers/ldservicesv2"
 	"net/http/httptest"
 	"testing"
 	"time"
@@ -19,40 +19,18 @@ import (
 )
 
 func TestFDV2DefaultDataSourceIsStreaming(t *testing.T) {
-	requireIntent := func(t *testing.T, code string, reason string) httphelpers.SSEEvent {
-		intent := datasourcev2.ServerIntent{Payloads: []datasourcev2.Payload{
-			{ID: "fake-id", Target: 0, Code: code, Reason: reason},
-		}}
-		intentData, err := json.Marshal(intent)
-		require.NoError(t, err)
-		return httphelpers.SSEEvent{
-			Event: "server-intent",
-			Data:  string(intentData),
-		}
-	}
-
-	requireTransferred := func(t *testing.T) httphelpers.SSEEvent {
-		type payloadTransferred struct {
-			State   string `json:"state"`
-			Version int    `json:"version"`
-		}
-		transferredData, err := json.Marshal(payloadTransferred{State: "[p:17YNC7XBH88Y6RDJJ48EKPCJS7:53]", Version: 1})
-		require.NoError(t, err)
-		return httphelpers.SSEEvent{
-			Event: "payload-transferred",
-			Data:  string(transferredData),
-		}
-	}
-
-	intent := requireIntent(t, "xfer-full", "payload-missing")
-
 	data := ldservices.NewServerSDKData().Flags(&alwaysTrueFlag)
 
-	streamHandler, streamSender := ldservices.ServerSideStreamingServiceHandler(intent)
-	for _, object := range data.ToPutObjects() {
-		streamSender.Enqueue(object)
-	}
-	streamSender.Enqueue(requireTransferred(t))
+	protocol := ldservicesv2.NewStreamingProtocol().
+		WithIntent(datasourcev2.ServerIntent{Payloads: []datasourcev2.Payload{
+			{ID: "fake-id", Target: 0, Code: "xfer-full", Reason: "payload-missing"},
+		}}).
+		WithPutObjects(data.ToBaseObjects()).
+		WithTransferred()
+
+	streamHandler, streamSender := ldservices.ServerSideStreamingServiceHandler(protocol.Next())
+
+	protocol.Enqueue(streamSender)
 
 	httphelpers.WithServer(streamHandler, func(streamServer *httptest.Server) {
 		logCapture := ldlogtest.NewMockLog()
