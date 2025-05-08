46329d4fa16165fc70d8abd37b8d89c23f6e57d3
Matthew Keeler <mkeeler@launchdarkly.com>
2025-05-08 12:20:26 -0400

    Add some disclaimers in case we have to make changes

--- subsystems/payloads.go
+++ subsystems/payloads.go
@@ -1,6 +1,12 @@
 package subsystems
 
 // Payload represents a payload delivered in a streaming response.
+//
+// This type is not stable, and not subject to any backwards
+// compatibility guarantees or semantic versioning. It is not suitable for production usage.
+//
+// Do not use it.
+// You have been warned.
 type Payload struct {
 	// The id here doesn't seem to match the state that is included in the
 	// Payload transferred object.
@@ -14,6 +20,12 @@ type Payload struct {
 }
 
 // PollingPayload represents a payload that is delivered in a polling response.
+//
+// This type is not stable, and not subject to any backwards
+// compatibility guarantees or semantic versioning. It is not suitable for production usage.
+//
+// Do not use it.
+// You have been warned.
 type PollingPayload struct {
 	// Note: the first event in a PollingPayload should be a Payload.
 	Events []RawEvent `json:"events"`
--- subsystems/raw_event.go
+++ subsystems/raw_event.go
@@ -6,6 +6,12 @@ import (
 
 // RawEvent is a partially deserialized event that allows the event name to be extracted before
 // the rest of the event is deserialized.
+//
+// This type is not stable, and not subject to any backwards
+// compatibility guarantees or semantic versioning. It is not suitable for production usage.
+//
+// Do not use it.
+// You have been warned.
 type RawEvent struct {
 	Name EventName       `json:"event"`
 	Data json.RawMessage `json:"data"`
