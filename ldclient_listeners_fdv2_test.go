c9f52086555cdb1a692e86df3c4a214f12b63d94
Matthew Keeler <mkeeler@launchdarkly.com>
2025-06-12 10:08:01 -0400

    add big segment test

--- ldclient_listeners_fdv2_test.go
+++ ldclient_listeners_fdv2_test.go
@@ -2,6 +2,7 @@ package ldclient
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/launchdarkly/go-server-sdk/v7/interfaces"
 	"github.com/launchdarkly/go-server-sdk/v7/internal/sharedtest"
+	"github.com/launchdarkly/go-server-sdk/v7/internal/sharedtest/mocks"
 	"github.com/launchdarkly/go-server-sdk/v7/subsystems"
 	"github.com/stretchr/testify/assert"
 
@@ -234,5 +236,45 @@ func TestDataSourceStatusProviderV2(t *testing.T) {
 			assert.WithinDuration(t, time.Now(), start, time.Millisecond*30)
 			assert.False(t, foundIt)
 		}, httphelpers.HandlerWithStatus(401))
+	})
+}
+
+func TestBigSegmentsStoreStatusProviderV2(t *testing.T) {
+	t.Run("returns unavailable status when not configured", func(t *testing.T) {
+		clientListenersV2Test(func(p clientListenersV2TestParams) {
+			assert.Equal(t, interfaces.BigSegmentStoreStatus{},
+				p.client.GetBigSegmentStoreStatusProvider().GetStatus())
+		})
+	})
+
+	t.Run("sends status updates", func(t *testing.T) {
+		store := &mocks.MockBigSegmentStore{}
+		store.TestSetMetadataToCurrentTime()
+		storeFactory := mocks.SingleComponentConfigurer[subsystems.BigSegmentStore]{Instance: store}
+		clientListenersV2TestWithConfig(
+			func(c *Config) {
+				c.BigSegments = ldcomponents.BigSegments(storeFactory).StatusPollInterval(time.Millisecond * 10)
+			},
+			func(p clientListenersV2TestParams) {
+				statusCh := p.client.GetBigSegmentStoreStatusProvider().AddStatusListener()
+
+				mocks.ExpectBigSegmentStoreStatus(
+					t,
+					statusCh,
+					p.client.GetBigSegmentStoreStatusProvider().GetStatus,
+					time.Second,
+					interfaces.BigSegmentStoreStatus{Available: true},
+				)
+
+				store.TestSetMetadataState(subsystems.BigSegmentStoreMetadata{}, errors.New("failing"))
+
+				mocks.ExpectBigSegmentStoreStatus(
+					t,
+					statusCh,
+					p.client.GetBigSegmentStoreStatusProvider().GetStatus,
+					time.Second,
+					interfaces.BigSegmentStoreStatus{Available: false},
+				)
+			})
 	})
 }
